9ce24d79cb9546fc78530d9e62fbab7fadcd06b1
sokkeangYeang <sokkeang168861@gmail.com>
2026-06-17 19:48:10 +0700

    add more

--- finance-tracker/frontend/src/main.js
+++ finance-tracker/frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios' 
+axios.defaults.baseURL = 'http://172.16.16.80:5000';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+
+app.mount('#app')
